2b6ce1f7792c0c31326974bb795f1c5ec041c5bf
Stephen Blackstone <sblackstone@gmail.com>
2018-04-06 14:26:11 -0400

    Remove cruft

--- js/controller.js
+++ js/controller.js
@@ -6,19 +6,11 @@ const Controller = function() {
 
 
 Controller.prototype.init_handlers = function() {
-    $('.square').on("click", (o) => {
-      let square_number = parseFloat($(o.target).closest(".square").data("square"));
-      this.model.add_to_human_move(square_number);
-      
-//      $('.square').removeClass('highlight');
-//      this.mv.slide_moves[square_number].forEach((i) => {
-//        this.model.view.highlight_square(i);        
-//      });
-//      this.model.view.highlight_square(square_number);
-
-      console.log(square_number);
-    });
-  
+  $('.square').on("click", (o) => {
+    let square_number = parseFloat($(o.target).closest(".square").data("square"));
+    this.model.add_to_human_move(square_number);
+    console.log(square_number);
+  });
 };
 
 
