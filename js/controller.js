4c3f50bebf6651839c4499270d37e8038d1e2e92
Stephen Blackstone <sblackstone@gmail.com>
2018-04-06 14:23:01 -0400

    Convert Controller

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,11 @@
-class Controller {
-  constructor() {
-    console.log("*** Controller Init");
-    this.model = new Model();
-    this.init_handlers();
-  }
+const Controller = function() {
+  console.log("*** Controller Init");
+  this.model = new Model();
+  this.init_handlers();  
+};
 
-  init_handlers() {
+
+Controller.prototype.init_handlers = function() {
     $('.square').on("click", (o) => {
       let square_number = parseFloat($(o.target).closest(".square").data("square"));
       this.model.add_to_human_move(square_number);
@@ -18,9 +18,9 @@ class Controller {
 
       console.log(square_number);
     });
-  }
+  
+};
 
-}
 
 $(function() {
   c = new Controller();  
