37b7cab3290675d986b2a2c4f8bb17ab65eb8ce1
Stephen Blackstone <sblackstone@gmail.com>
2018-05-01 22:40:59 -0400

    Heuristic function o(n) -> o(1)

--- js/model.js
+++ js/model.js
@@ -71,22 +71,13 @@ Model.prototype.heuristic = function(maximizingPlayer) {
   if (this.winner() === bad_side) {
     return(-Infinity);
   }
-
+  
   let score = 0;
-  for (let i = 0; i < 64; i++) {
-    
-    if (this.square(i) == good_side) {
-      score += 1;
-      if (this.is_court_square(i)) {
-        score += 1;
-      }
-    } else if (this.square(i) == bad_side) {
-      score -= 1.5;
-      if (this.is_court_square(i)) {
-        score -= 1.25;
-      }
-    }
-  }
+  score += this._state.square_tracker[good_side];
+  score += this._state.court_tracker[good_side];
+  score -= this._state.square_tracker[bad_side]*1.5;
+  score -= this._state.court_tracker[bad_side]*1.25;
+
   return(score);
 };
 
@@ -403,14 +394,21 @@ Model.prototype.is_game_started = function() {
 
 Model.prototype.set_square = function(i, val) {
   // Keep track of how many pieces are in center for fast win-condition checks..
-  if (this.is_court_square(i)) {
-    if (this.is_occupied(i) && val === 0) {
+  // Also keep track of how many pieces each player has.
+  if (this.is_occupied(i) && val === 0) {
+    this._state.square_tracker[this.square(i)] -= 1;
+    if (this.is_court_square(i)) {
       this._state.court_tracker[this.square(i)] -= 1;
     };
-    if (this.square(i) === 0 && val > 0) {
+  };
+
+  if (this.square(i) === 0 && val > 0) {
+    this._state.square_tracker[val] += 1;
+    if (this.is_court_square(i)) {
       this._state.court_tracker[val] += 1;
     };
   };
+
   this._state.squares[i] = val;  
 };
 
@@ -438,6 +436,7 @@ Model.prototype.initialize_state = function(human_team = 1, computer_level = 1) 
   this._state.committed_moves  = [];
   this._state.error_message    = null;
   this._state.court_tracker    = [0,0,0];
+  this._state.square_tracker   = [0,24,24];
   this._state.squares          = [ 1, 2, 1, 2, 1, 2, 1, 2,
                                    2, 1, 2, 1, 2, 1, 2, 1, 
                                    1, 2, 0, 0, 0, 0, 1, 2,
